4c2315c0c987e25918fe9028a517dd171545867c
Paweł Kubrak <peku33@gmail.com>
2017-06-01 01:26:32 +0200

    Dodanie kdd.calculate_data_distances

--- kddTest.r
+++ kddTest.r
@@ -1,5 +1,6 @@
 source('kddLoader.r')
 source('distance.r')
+source('knn.r')
 
 kdd.namesfile <- kdd.namesfile_read()
 
@@ -8,15 +9,11 @@ kdd.namesfile <- kdd.namesfile_read()
 
 kdd.columns <- kdd.namesfile_columns_extract(kdd.namesfile)
 kdd.columns <- kdd.columns_add_label(kdd.columns)
-kdd.data <- kdd.data_read('kddcup.data_10_lines_labeled', kdd.columns)
+kdd.data <- kdd.data_read('kddcup.data_10k_lines_labeled', kdd.columns)
 
-matrix <- distance.mixed_as_continuous_matrix(
-	kdd.data[1, kdd.columns$is_continuous],
-	kdd.data[1, !kdd.columns$is_continuous],
+kdd.calculate_data_distances <- function(data.test.row, data.train.rows) {
 
-	kdd.data[7, kdd.columns$is_continuous],
-	kdd.data[7, !kdd.columns$is_continuous],
+	distance.multiple.mixed_as_continuous(data.test.row, data.train.rows, kdd.columns$is_continuous, !kdd.columns$is_continuous, 0, 1)
 
-	100,
-	1000
-)
+}
+
