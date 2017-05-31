573ff1b985e825aee8c5f189fdad3d1355fad3c0
Paweł Kubrak <peku33@gmail.com>
2017-06-01 01:26:12 +0200

    Wstępna implementacja funkcji knn

---
+++ knn.r
@@ -0,0 +1,32 @@
+knn.data_frame <- function(data, data.column.distance, data.column.data, k) {
+
+	# Posortuj wynik po kolumnie z odległością
+	# Weź k pierwszych indeksów
+	order_k <- order(data[data.column.distance])[1:k]
+
+	# Weź k pierwszyc
+	data_k <- data[order_k, data.column.data]
+
+	data_k
+}
+
+knn.data_frame.continuous <- function(data, data.column.distance, data.column.data, k) {
+
+	data_k <- knn.data_frame(data, data.column.distance, data.column.data, k)
+
+	value_average <- sum(data_k) / k
+	value_average
+}
+
+knn.data_frame.discrete <- function(data, data.column.distance, data.column.data, k) {
+
+	data_k <- knn.data_frame(data, data.column.distance, data.column.data, k)
+	table(data_k)
+}
+
+knn.data_frame.discrete.ordered <- function(data, data.column.distance, data.column.data, k) {
+
+	data_k <- knn.data_frame.discrete(data, data.column.distance, data.column.data, k)
+	data_k[order(data_k, decreasing = TRUE)]
+
+}
