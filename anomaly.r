70a41f5a6fb2e3c37c88a8602e229628668d4d31
Paweł Kubrak <peku33@gmail.com>
2017-06-05 02:18:12 +0200

    Podział anomaly.score.data.multiple.k.matrix na .build i .by Uzupełnienie dokumentacji

--- anomaly.r
+++ anomaly.r
@@ -1,8 +1,9 @@
 source('knn.r')
+source('distance.r')
 
-# Posiadając macierz odległości punktu testowego od danych normalnych oblicza współczynnik podobieństwa
+# Posiadając wektor odległości punktu testowego od danych normalnych oblicza współczynnik podobieństwa
 # Współczynnik jest obliczany jako suma odległości od k najbliższych sąsiadów
-anomaly.score.single.k.dist <- function(distances.vector, k) {
+anomaly.score.vector.k <- function(distances.vector, k) {
 
 	# Znajdź indeksy k najbliższych danych normalnych
 	normal.rows.k_indexes <- knn.vector.get_indexes(distances.vector, k)
@@ -14,14 +15,53 @@ anomaly.score.single.k.dist <- function(distances.vector, k) {
 	sum(normal.rows.k_distances)
 }
 
-# J/w, jednak przyjmując na wejściu dane, a nie wektor odległości
-# Wewnętrznie wykonuje obliczenie wektora odległości
-anomaly.score.single.k <- function(test.row, normal.rows, k) {
+# Dla podanego zbioru danych testowych oraz trenujących buduje macierz odległości
+# Rozdzielenie implementacji pozwala na szybkie wykorzystanie tej samej macierzy z różnymi wartościami k
+anomaly.score.data.multiple.k.matrix.build <- function(test.rows, train.rows.normal) {
 
-	# Oblicz odległości od poszczególnych danych normalnych
-	distances.vector <- distance.data_frame.single.continuous.2(test.row, normal.rows)
+	# Oblicz macierz odległości pomiędzy danymi testowymi a trenującymi.
+	# Jeden wiersz macierzy dla jednego wektora z danych testowych
+	# Kolejne kolumny macierzy to odległości od kolejnych danych trenujących
+	distance.data_frame.multiple.continuous.1(
+		test.rows,
+		train.rows.normal
+	)
+}
 
-	# Zwróć wynik
-	anomaly.score.single.k.dist(distances.vector)
+# Dla podanej macierzy odległości oraz zbioru danych testowych i parametru k buduje wektor miar niepodobieństw.
+# Każdy wektor ze zbioru danych testowych to jeden element wyjściowego wektora miar.
+# Wewnętrznie użyta została metoda KNN, do której parametr k jest przekazywany
+anomaly.score.data.multiple.k.matrix.by <- function(distances.matrix, test.rows, k) {
 
+	# Zbieramy wartości współczynnika nieprawidłowości dla danych prawidłowych i nieprawidłowych
+	distances <- c()
+
+	for(test.row.id in 1:nrow(test.rows)) {
+
+		# Z macierzy odległości wybierz wiersz odpowiadający tej danej testowej
+		distances.vector <- distances.matrix[test.row.id, ]
+
+		# Oblicz miarę nieprawidłowości dla k najbliższych sąsiadów
+		distances.sum <- anomaly.score.vector.k(distances.vector, k)
+
+		# Dodaj wynik do list
+		distances <- c(distances, distances.sum)
+	}
+
+	# Zwróć listę
+	distances
+}
+
+# Właściwa metoda obliczająca wektor miary niepodobieństw
+# Łączy metody .build i .by
+anomaly.score.data.multiple.k <- function(test.rows, train.rows.normal, k) {
+
+	anomaly.score.data.multiple.k.matrix.by(
+		anomaly.score.data.multiple.k.matrix.build(
+			test.rows,
+			train.rows.normal
+		),
+		test.rows,
+		k
+	)
 }
