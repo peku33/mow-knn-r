8aaaeb8dfd40e9576b9d48d26cfa7de0642b80f6
Paweł Kubrak <peku33@gmail.com>
2017-06-05 02:19:21 +0200

    Uzupełnienie metod testowania

--- kdd/kdd_test.r
+++ kdd/kdd_test.r
@@ -1,72 +1,70 @@
 source('kdd/kdd_loader.r')
+source('anomaly.r')
 source('distance.r')
 source('knn.r')
 source('cid.r')
+
+library('pROC')
 
 kdd.load.data <- function(kdd.data.filename, kdd.columns) {
 
 	kdd.data <- kdd.data.read(kdd.data.filename, kdd.columns)
 
 	# print('Changing discrete -> continuous')
-	# Ogranicz dane tylko do atrybutów ciągłych, docelowo - zamień dane na dane ciągłe
-	# kdd.data.continuous <- kdd.data[, kdd.columns$is_continuous]
 
 	# print('Normalizing data')
 	kdd.data <- normalizer.set.scale_continuous(kdd.data, kdd.columns$is_continuous)
 
+	# Ogranicz dane tylko do atrybutów ciągłych, docelowo - zamień dane na dane ciągłe
+	# kdd.data.continuous <- kdd.data[, kdd.columns$is_continuous]
+
+	#kdd.data.continuous
 	kdd.data
 }
 
-kdd.validate.train.test.knn <- function(test.rows, train.rows, k) {
+kdd.data.sets.num <- 10
+kdd.k.values <- c(1, 2, 5, 10, 25, 50, 100)
 
-	# Dane trenujące oznaczone jako normalne służą do obliczania współczynnika nieprawidłowości
-	# Wyciągamy tylko te dane trenujące, oznaczone jako normalne
-	train.rows.normal <- train.rows[train.rows$label == "normal.", ]
-
-	# Oblicz macierz odległości pomiędzy danymi testowymi a trenującymi.
-	# Jeden wiersz macierzy dla jednego wektora z danych testowych
-	# Kolejne kolumny macierzy to odległości od kolejnych danych trenujących
-	distances.matrix <- distance.data_frame.multiple.continuous.1(test.rows, train.rows.normal)
-
-	# Zbieramy wartości współczynnika nieprawidłowości dla danych prawidłowych i nieprawidłowych
-	knn.distances.normal <- c()
-	knn.distances.not_normal <- c()
-
-	for(test.row.id in 1:nrow(test.rows)) {
-
-		# Z macierzy odległości wybierz wiersz odpowiadający tej danej testowej
-		distances.test_row.to.test_rows <- distances.matrix[test.row.id, ]
-
-		# Weź indeksy k najbliższych danych
-		distances.test_row.to.test_rows.knn.indexes <- knn.vector.get_indexes(distances.test_row.to.test_rows, k)
-	
-		# Oblicz miarę nieprawidłowości testowego wektora jako sumę odległości k najbliższych sąsiadów
-		distances.test_row.to.test_rows.knn.distance <- sum(distances.test_row.to.test_rows[distances.test_row.to.test_rows.knn.indexes])
-
-		if(test.rows[test.row.id, ]$label == "normal.")
-			knn.distances.normal <- c(knn.distances.normal, distances.test_row.to.test_rows.knn.distance)
-		else
-			knn.distances.not_normal <- c(knn.distances.not_normal, distances.test_row.to.test_rows.knn.distance)
-	}
-
-	list(knn.distances.normal, knn.distances.not_normal)
-}
-
-kdd.data.test.filename <- 'kdd/data/samples/kddcup.data_sample.1'
-kdd.data.train.filename <- 'kdd/data/samples/kddcup.data_sample.2'
-kdd.k <- 100
-
-print('Loading namesfile')
+print('Loading namesfile:')
 kdd.namesfile <- kdd.namesfile.read('kdd/data/kddcup.names')
 
 kdd.columns <- kdd.namesfile.columns.extract(kdd.namesfile)
 kdd.columns <- kdd.columns.add.label(kdd.columns)
 
-print('Loading datafiles')
-kdd.data.test <- kdd.load.data(kdd.data.test.filename, kdd.columns)
-kdd.data.train <- kdd.load.data(kdd.data.train.filename, kdd.columns)
+print('Loading datafiles:')
+kdd.data.sets <- list()
+for(i in 1:kdd.data.sets.num) {
+	kdd.data.set.filename <- sprintf('kdd/data/samples/kddcup.data_sample.%d', i)
+	kdd.data.sets[[i]] <- kdd.load.data(kdd.data.set.filename, kdd.columns)
+}
 
-print('Running evaluation')
-kdd.result <- kdd.validate.train.test.knn(kdd.data.test, kdd.data.train, kdd.k)
+print('Running cross evaluation:')
+# plot.new()
+for(index.train in 1:(kdd.data.sets.num-1)) {
+	for(index.test in (index.train+1):kdd.data.sets.num) {
 
-print('Done')
+		print(sprintf(' Train set = %d, Test set = %d', index.train, index.test))
+		matrix <- anomaly.score.data.multiple.k.matrix.build(
+			kdd.data.sets[[index.test]][, kdd.columns$is_continuous],
+			kdd.data.sets[[index.train]][kdd.data.sets[[index.train]]$label == "normal.", kdd.columns$is_continuous]
+		)
+
+		for(k in kdd.k.values) {
+
+			print(sprintf('  k = %d', k))
+			name <- sprintf('Training set = %d, Testing set = %d, K = %d', index.train, index.test, k)
+			distances <- anomaly.score.data.multiple.k.matrix.by(matrix, kdd.data.sets[[index.test]], k)
+
+			roc <- roc(
+				factor(kdd.data.sets[[index.test]]$label != "normal.", ordered = TRUE),
+				factor(distances, ordered = TRUE),
+			)
+
+			#roc.auc <- auc(roc)
+			#subname <- sprintf('AUC = %f', roc.auc)
+
+			plot.roc(roc, main = name, print.auc = TRUE)
+		}
+	}
+}
+dev.off()
